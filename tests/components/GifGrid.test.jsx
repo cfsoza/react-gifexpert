7a165cb09ed8890d8056332807c1bcd88cbc7607
Carlos Soza Chavarría <csoza@people-apps.com>
2023-11-14 11:31:29 -0600

    pruebas testing

---
+++ tests/components/GifGrid.test.jsx
@@ -0,0 +1,49 @@
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../src/components/GifGrid";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock('../../src/hooks/useFetchGifs');
+
+describe('Pruebas en GifGrid', () => { 
+    const category = 'One Punch';
+
+    test('debe de mostrar el loading inicialmente', () => { 
+
+        useFetchGifs.mockReturnValue({
+            images:[],
+            isLoading: true
+        });
+
+        render(<GifGrid category = { category }/>);
+
+        expect(screen.getByText('Cargado....'));
+        expect(screen.getByText(category));
+     });
+
+     test('debe de mostrar items cuando se cargan las imagenes mediante useFetchGifs', () => { 
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Saitama',
+                url: 'https://localhist/saitama.jpg'
+             },
+             {
+                id: '123',
+                title: 'Goku',
+                url: 'https://localhist/goku.jpg'
+             }
+        ];
+
+
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render(<GifGrid category = { category }/>);
+
+        expect(screen.getAllByRole('img').length).toBe(2);
+      });
+ });
+
+
